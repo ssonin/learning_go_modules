8bf2fc097999274643b67e93bb632538af6928d4
Sergei Sonin <sonin.sm@gmail.com>
2025-04-25 22:55:24 +0100

    Add v2

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ssonin/learning_go_modules
 
-go 1.23
+go 1.23.0
+
+toolchain go1.24.2
+
+require golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0
--- lib.go
+++ lib.go
@@ -3,9 +3,18 @@
 // [Learning Go]: https://www.amazon.com/Learning-Go-Jon-Bodner-ebook/dp/B0CS5DY1VN/ref=tmm_kin_swatch_0
 package learning_go_modules
 
-// Add [adds] two integers
+import (
+	"golang.org/x/exp/constraints"
+)
+
+// Number is a type that permits any integer or floating-point type
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add [adds] two numbers
 //
 // [adds]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
