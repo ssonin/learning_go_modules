97485093523c032e13ce27e3e02703925ae443ec
Sergei Sonin <sonin.sm@gmail.com>
2025-04-25 20:10:16 +0100

    Add docs

--- lib.go
+++ lib.go
@@ -1,5 +1,11 @@
+// Package learning_go_modules contains learning exercises from the chapter 10 of the [Learning Go] book
+//
+// [Learning Go]: https://www.amazon.com/Learning-Go-Jon-Bodner-ebook/dp/B0CS5DY1VN/ref=tmm_kin_swatch_0
 package learning_go_modules
 
+// Add [adds] two integers
+//
+// [adds]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
